5713310070d80489a78b4dfdd221166f7f67e447
Shiroke-013 <Shiroke4321@gmail.com>
2023-02-16 21:12:54 -0500

    List of Professors and Courses implemented.

--- academic-record-web/src/main/java/com/java/controllers/CourseController.java
+++ academic-record-web/src/main/java/com/java/controllers/CourseController.java
@@ -1,14 +1,25 @@
 package com.java.controllers;
 
+import com.java.services.CourseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/courses")
 @Controller
 public class CourseController {
 
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
     @RequestMapping({"", "/", "/list", "/list.html"})
-    public String courseList(){
+    public String courseList(Model model){
+
+        model.addAttribute("courses", courseService.findAll());
+
         return "courses/list";
     }
 }
--- academic-record-web/src/main/java/com/java/controllers/ProfessorController.java
+++ academic-record-web/src/main/java/com/java/controllers/ProfessorController.java
@@ -1,14 +1,25 @@
 package com.java.controllers;
 
+import com.java.services.ProfessorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/professors")
 @Controller
 public class ProfessorController {
 
+    private final ProfessorService professorService;
+
+    public ProfessorController(ProfessorService professorService) {
+        this.professorService = professorService;
+    }
+
     @RequestMapping({"", "/", "/list", "/list.html"})
-    public String professorList(){
+    public String professorList(Model model){
+
+        model.addAttribute("professors", professorService.findAll());
+
         return "professors/list";
     }
 }
