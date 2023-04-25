afcb5808bdc03c5d5adb3dbd778f59ffdc4a7f8e
Shiroke-013 <Shiroke4321@gmail.com>
2023-04-25 11:25:18 -0500

    Relations

--- academic-record-data/src/main/java/com/java/model/Student.java
+++ academic-record-data/src/main/java/com/java/model/Student.java
@@ -1,8 +1,8 @@
 package com.java.model;
 
 import com.java.dto.StudentDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.sun.tools.javac.util.List;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,9 +16,16 @@ import java.util.Date;
 @Table(name = "students")
 public class Student extends User {
 
+    @OneToMany(mappedBy = "student")
+    private List<Grade> grades;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinTable(name = "stu_course",
+        joinColumns =
+                { @JoinColumn(name = "student_id", referencedColumnName = "id") },
+        inverseJoinColumns =
+                { @JoinColumn(name = "course_id", referencedColumnName = "id") })
     private Course course;
-
-
 
     public Student(String firstName, String lastname, String email, String password){
         this.setFirstName(firstName);
